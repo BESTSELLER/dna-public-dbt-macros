54cc8f395e88cee4c65e364b7e046b354ac42e50
Martha Scheffler <martha.scheffler@bestseller.com>
2024-03-04 13:48:30 +0100

    fix: if no schemas excluded

--- macros/cleanup/get_exception_schemas.sql
+++ macros/cleanup/get_exception_schemas.sql
@@ -6,11 +6,13 @@
 
   {% set excluded_schemas = [] %}
   {% set exclude_schemas = dbt_utils.get_column_values(table=ref('cleanup_exceptions'), column='schema_name', where="object_type='schema'") %}
+  {% if exclude_schemas %}
   {% for schema in exclude_schemas %}
     {% set msg ='Found schema in exceptions: ' ~ schema.upper() %}
     {{ log( msg , info=true) }}
     {% do excluded_schemas.append(schema.upper()) %}
   {% endfor %}
+  {% endif %}
 
   {{ return(excluded_schemas) }}
 
